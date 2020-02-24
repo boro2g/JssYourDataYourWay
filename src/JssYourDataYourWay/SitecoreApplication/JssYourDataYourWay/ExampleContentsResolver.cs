fcb99bc00792aba8df7c740aecb8df478868f0c3
nick hills <boro2g@gmail.com>
2020-02-24 16:21:11 +0000

    Added example contents resolver

---
+++ src/JssYourDataYourWay/SitecoreApplication/JssYourDataYourWay/ExampleContentsResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Specialized;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.Mvc.Presentation;
+
+namespace SitecoreApplication.JssYourDataYourWay
+{
+    public class ExampleContentsResolver : IRenderingContentsResolver
+    {
+        public bool IncludeServerUrlInMediaUrls { get; set; }
+        public bool UseContextItem { get; set; }
+        public string ItemSelectorQuery { get; set; }
+        public NameValueCollection Parameters { get; set; }
+
+        public object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            //if you want to access the datasource item
+            //var datasource = !string.IsNullOrEmpty(rendering.DataSource)
+            //    ? rendering.RenderingItem?.Database.GetItem(rendering.DataSource)
+            //    : null;
+
+            return new
+            {
+                name = "test",
+                date = DateTime.Now,
+                hello = "world"
+            };
+        }
+    }
+}
