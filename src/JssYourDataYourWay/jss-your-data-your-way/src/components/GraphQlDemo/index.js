ef4c0e7eb4bb9e0a5d63e73aca19bc88ff153c17
nick hills <boro2g@gmail.com>
2020-02-24 17:36:11 +0000

    Adding graphql example

---
+++ src/JssYourDataYourWay/jss-your-data-your-way/src/components/GraphQlDemo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+
+const GraphQlDemo = (props) => (
+  <div>
+    <p>GraphQlDemo Component</p>
+    
+  </div>
+);
+
+export default GraphQlDemo;
