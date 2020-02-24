3b818348f59786c15acc9a7011b5947e77216525
nick hills <boro2g@gmail.com>
2020-02-24 15:35:25 +0000

    Added jss and context extension example

---
+++ src/JssYourDataYourWay/SitecoreApplication/JssYourDataYourWay/ExampleContextExtension.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.JavaScriptServices.Configuration;
+using Sitecore.JavaScriptServices.ViewEngine.LayoutService.Pipelines.GetLayoutServiceContext;
+using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
+
+namespace SitecoreApplication.JssYourDataYourWay
+{
+    public class ExampleContextExtension : JssGetLayoutServiceContextProcessor
+    {
+        public ExampleContextExtension(IConfigurationResolver configurationResolver) : base(configurationResolver)
+        {
+        }
+
+        protected override void DoProcess(GetLayoutServiceContextArgs args, AppConfiguration application)
+        {
+            args.ContextData.Add("securityInfo", new
+            {
+                isAnonymous = !Sitecore.Context.User.IsAuthenticated
+            });
+        }
+    }
+}
--- src/JssYourDataYourWay/SitecoreApplication/readme.txt
+++ src/JssYourDataYourWay/SitecoreApplication/readme.txt
@@ -1,4 +1,4 @@
 ﻿Setup Nuget feed
 Followed https://doc.sitecore.com/developers/90/sitecore-experience-manager/en/set-up-sitecore-and-visual-studio-for-development.html
-Installed JSS
+Installed JSS https://dev.sitecore.net/Downloads/Sitecore_JavaScript_Services.aspx and added dlls to library (rather than nuget) as they didn't work
 https://sitecore93sc.dev.local/sitecore/api/layout/render/jss?item=/&sc_apikey={D9D0F9D9-2498-45BC-BF8E-371ED9529121}
