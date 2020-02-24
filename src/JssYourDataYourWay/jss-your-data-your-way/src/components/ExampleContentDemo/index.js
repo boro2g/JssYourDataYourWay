10c252bee0331448cbcf147c948f05432a9bafa2
nick hills <boro2g@gmail.com>
2020-02-24 16:44:10 +0000

    Added custom component

---
+++ src/JssYourDataYourWay/jss-your-data-your-way/src/components/ExampleContentDemo/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from '@sitecore-jss/sitecore-jss-react';
+
+const ExampleContentDemo = (props) => (
+  <div>
+    <p>ExampleContentDemo Component</p>
+    <Text field={props.fields.heading} />
+    <p>{props.fields.date}</p>
+  </div>
+);
+
+export default ExampleContentDemo;
