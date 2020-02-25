456cde0733620c6c4c8f3483fcdfc5c88f4d0d18
nick hills <boro2g@gmail.com>
2020-02-25 12:06:19 +0000

    Added proxy resolver example

---
+++ src/JssYourDataYourWay/SitecoreApplication/JssYourDataYourWay/ProxyContentsResolver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Sitecore.LayoutService.Configuration;
+using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+using Sitecore.Mvc.Presentation;
+
+namespace SitecoreApplication.JssYourDataYourWay
+{
+    public class ProxyContentsResolver : IRenderingContentsResolver
+    {
+        public bool IncludeServerUrlInMediaUrls { get; set; }
+        public bool UseContextItem { get; set; }
+        public string ItemSelectorQuery { get; set; }
+        public NameValueCollection Parameters { get; set; }
+
+        public object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            var timeoutMs = int.Parse(rendering.Parameters?["Timeout"] ?? "500");
+
+            var url = rendering.Parameters?["Source Url"];
+
+            if (String.IsNullOrWhiteSpace(url))
+            {
+                return new ProxyContentResult(false, "No url set on rendering parameters 'Source Url'");
+            }
+
+            var client = new HttpClient
+            {
+                Timeout = TimeSpan.FromMilliseconds(timeoutMs)
+            };
+
+            try
+            {
+                using (var message = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    using (var httpResponse = client.GetAsync(url).Result)
+                    {
+                        return new ProxyContentResult(true, JsonConvert.DeserializeObject<ContentModel>(httpResponse.Content.ReadAsStringAsync().Result));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return new ProxyContentResult(false, $"Timeout - {e.Message}.");
+            }
+        }
+
+        public class ProxyContentResult
+        {
+            public ProxyContentResult(bool success, string content)
+            {
+                Success = success;
+                Content = new ContentModel { Content = content };
+            }
+
+            public ProxyContentResult(bool success, ContentModel content)
+            {
+                Success = success;
+                Content = content;
+            }
+
+            public bool Success { get; set; }
+            public ContentModel Content { get; set; }
+        }
+
+        public class ContentModel
+        {
+            public string Content { get; set; }
+            public string Source { get; set; }
+            public int DelayMs { get; set; }
+        }
+    }
+}
--- src/JssYourDataYourWay/SitecoreApplication/readme.txt
+++ src/JssYourDataYourWay/SitecoreApplication/readme.txt
@@ -6,6 +6,12 @@ https://sitecore93sc.dev.local/sitecore/api/layout/render/jss?item=/&sc_apikey={
 GraphQl
 https://sitecore93sc.dev.local/sitecore/api/graph/items/master/ui?sc_apikey=%7BD9D0F9D9-2498-45BC-BF8E-371ED9529121%7D&query=%7B%0A%20%20item(path%3A%20%22%2Fsitecore%2Fcontent%2Fhome%22)%20%7B%0A%20%20%20%20id%0A%20%20%20%20...%20on%20SampleItem%20%7B%0A%20%20%20%20%20%20text%20%7B%0A%20%20%20%20%20%20%20value%20%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D
 
+Azure function:
+https://jssyourdatayourway.azure.boro2g.co.uk/api/JssYourDataYourWay
+Lambda function:
+https://jssyourdatayourway.aws.boro2g.co.uk/api/jss/content
+https://eu-west-1.console.aws.amazon.com/lambda/home?region=eu-west-1#/functions/jssyourdatayourway-AspNetCoreFunction-1W8OG4SHRVSK4?tab=configuration
+
 Jss
 npm install -g @sitecore-jss/sitecore-jss-cli
 jss create jss-your-data-your-way react
