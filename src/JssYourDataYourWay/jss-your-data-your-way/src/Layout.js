cd4da9c0b9ef6915d0b61b4ec8de729a04e00de7
Nick Hills <Nick.Hills@valtech.com>
2020-03-02 11:39:31 +0000

    Added context demo

--- src/JssYourDataYourWay/jss-your-data-your-way/src/Layout.js
+++ src/JssYourDataYourWay/jss-your-data-your-way/src/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Placeholder, VisitorIdentification } from '@sitecore-jss/sitecore-jss-react';
+import { Placeholder, VisitorIdentification, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
 import { NavLink } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import Helmet from 'react-helmet';
@@ -21,12 +21,15 @@ import logo from './assets/sc_logo.svg';
 // This is boilerplate navigation for sample purposes. Most apps should throw this away and use their own navigation implementation.
 // Most apps may also wish to use GraphQL for their navigation construction; this sample does not simply to support disconnected mode.
 let Navigation = ({ t, i18n }) => (
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
+  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">    
     <h5 className="my-0 mr-md-auto font-weight-normal">
       <NavLink to="/" className="text-dark">
         <img src={logo} alt="Sitecore" />
       </NavLink>
     </h5>
+    {/* <p>
+      Context info: {t.sitecoreContext.securityInfo.isAnonymous}
+    </p> */}
     <nav className="my-2 my-md-0 mr-md-3">
       <a
         className="p-2 text-dark"
@@ -46,10 +49,19 @@ let Navigation = ({ t, i18n }) => (
   </div>
 );
 
+let ContextDemo = (props) => (
+  <div data-e2e-class="styleguide-layout-tabs-tab"> 
+    <p className="header">Context demo</p>
+    <p>isAnonymous: {props.sitecoreContext.securityInfo.isAnonymous.toString()}</p>
+  </div>
+);
+
 // inject dictionary props (`t`) into navigation so we can translate it
 // NOTE: using this is needed instead of using i18next directly to keep
 // the component state updated when i18n state (e.g. current language) changes
 Navigation = withTranslation()(Navigation);
+
+ContextDemo = withSitecoreContext()(ContextDemo);
 
 const Layout = ({ route }) => (
   <React.Fragment>
@@ -69,10 +81,11 @@ const Layout = ({ route }) => (
     */}
     <VisitorIdentification />
 
-    <Navigation />
+    <Navigation />    
 
     {/* root placeholder for the app, which we add components to using route data */}
     <div className="container">
+      <ContextDemo />
       <Placeholder name="jss-main" rendering={route} />      
     </div>
   </React.Fragment>
